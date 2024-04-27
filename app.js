11eeea5a48eb3d338dc10001c99a736285fbb32f
ikow-g <muh.djatmieka@gmail.com>
2024-04-27 16:46:47 +0700

    learning (import,object destruction,make local server)

--- app.js
+++ app.js
@@ -1,4 +1,19 @@
+// //menggunakan require untuk load module. hanya bisa di nodejs
+const helpers = require('./helpers');
+const total = helpers.sum(2,2);
+
+//cara membuat server local
+const http = require('http');
+const server = http.createServer((req, res) => {
+    res.end("hello world from node js");
+})
+
+server.listen(3000);
+
+//object destruction
+//kita bisa pakai fungsi secara langsung dari module dengan cara:
+const {sum} = require('./helpers');
+// const total = sum(2,2);
 
 
-const total = sum(2,5);
 console.log("total = ", total);
