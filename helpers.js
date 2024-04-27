95983493eb0ec60c1f4c04fa62333f0fba2b2657
ikow-g <muh.djatmieka@gmail.com>
2024-04-27 16:46:05 +0700

    learning (export,function arrow)

--- helpers.js
+++ helpers.js
@@ -1,3 +1,17 @@
-function sum(a,b){
+// function sum(a,b){
+//     return a+b;
+// }
+
+//arrow functions
+const sum = (a,b) => {
     return a+b;
 }
+
+// //coba export fungsi agar bisa dipakai di file lain
+module.exports = {
+    sum
+}
+
+//simplified export. tinggal tambah exports
+// exports.sum = (a,b) => a+b;
+//function arrow tanpa {} bisa jika hanya ada 1 statement
